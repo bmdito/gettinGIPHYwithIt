755b72f07e86f8e4a625e444a000391386d9efbe
bdiddy <44068902+bmdito@users.noreply.github.com>
2019-01-24 02:44:46 -0800

    finished most javascript and jquery, addded a bit of css

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,4 +1,4 @@
-console.log("Hello World");
+
 
 var animalArray = ["cat", "dog", "hamster", "horse"];
 
@@ -6,64 +6,103 @@ $(document).ready(function(){
     console.log("ready!");
     console.log(animalArray[1]);
 
-// creates buttons for animals
+// creates buttons for all animals in Animals array
     function renderButtons () {
 
         $("#buttonSection").empty();
+        $("#animal-input").empty();
 
         for(var i = 0; i<animalArray.length; i++) {
             var newButt = $("<button>");
             newButt.addClass("animalButt");
             
-            var animalButt = newButt.text(animalArray[i]);
+            var animalButt = newButt.text(animalArray[i]).attr("data-name", animalArray[i]);
             $("#buttonSection").append(animalButt);
+            
         }
     }
     renderButtons();
 
-    function displayGifs() {
+    // displays the 10 gifs for whatever animal is clicked
+    function displayGifs(){
 
+        $("#imgArea").empty();
+        var theAnimal = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + theAnimal + "&api_key=0ajdovi4XsCficMr7nSecx8IFqZBcne9&limit=10";
         
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=0ajdovi4XsCficMr7nSecx8IFqZBcne9&q=cow&limit=10&offset=0&rating=G&lang=en"
-         
-
+        
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response){
-
-            // var imageUrl = data[0].url;
-
-            // var newImg = $("<img>");
-            // var animImg = newImg.attr("src", imageUrl);
-
-            // $(".imgDiv").append(animImg);
-
             console.log(response);
+            var results = response.data;
+            
+            for(var i =0; i<results.length; i++){
 
-        });
+                var gifDiv = $("<div>");
+                gifDiv.addClass("gif-box");
+
+                var newImg = $("<img>");
+                newImg.attr("src", results[i].images.fixed_height_still.url);
+                newImg.addClass("imgStyle");
+                newImg.addClass("gif");
+                newImg.attr("state", "still");
+                newImg.attr("data-still", results[i].images.fixed_height_still.url);
+                newImg.attr("data-animate", results[i].images.fixed_height.url);
+                gifDiv.append(newImg);
+                
+                $("#imgArea").prepend(gifDiv);
+                
+              
+                
+            }
+
+            // on click function to start and stop GIF's
+            
+            $(".gif").on("click", function(){
+                
+                if($(this).attr("state")==="still"){
+                    $(this).attr("state", "animating");
+                    $(this).attr("src", $(this).attr("data-animate"));
+                } else
+                if($(this).attr("state")==="animating"){
+                    $(this).attr("state", "still");
+                    $(this).attr("src", $(this).attr("data-still"));
+
+                }
+
+                console.log("you clicked!");
+            });
 
         
-    }
-    displayGifs();
+        });
 
 
-    // onclick event for clicking animal button
-    $(".animalButt").on("click", function(){
+        
+    };
 
+    
+   
+    
 
-    });
+    
+
+    // adds button when a new animal is added through text form/"lets go" submit button
 
     $("#add-animal").on("click", function(event){
 
         event.preventDefault();
 
         var newAnimal = $("#animal-input").val().trim();
+                
         animalArray.push(newAnimal);
         console.log(animalArray);
         renderButtons();
 
     });
+
+    $(document).on("click", ".animalButt", displayGifs);
 
 });
 
